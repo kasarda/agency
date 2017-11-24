5bddaa85eafd7d9bba740b47f0eb88bd48c34049
kasarda <kasardaafilip@gmail.com>
2017-11-24 11:15:36 +0100

    Update

--- src/app/Home/index.js
+++ src/app/Home/index.js
@@ -1,20 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Animation from 'animejs'
 
 import './Home.css'
 
-function Home() {
-    return (
-        <div id="Home" className="view">
-            <div className="wraper flex direction-col justify-center">
-                <div className="column flex">
-                    <div className="rule hidden-sm-down"></div>
-                    <h1>Digital <br />
-                        Creative Agency</h1>
-                </div>
 
+function ScrollButton() {
+
+    function onClick() {
+        console.log('Scroll Button clicked')
+    }
+
+    return (
+        <div id="ScrollButton" className="hidden-sm-down">
+            <div className="mouse flex justify-center align-center" onClick={onClick}>
+                <div className="wheel"></div>
             </div>
         </div>
     )
+}
+
+
+class Home extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            ready: false
+        }
+    }
+
+    componentDidMount() {
+
+        this.setState({
+            ready: true
+        })
+
+    
+    }
+
+
+    render() {
+
+        const show_if_ready = this.state.ready ? '' : 'hide'
+
+        return (
+            <div id="Home" className="view">
+
+                <svg viewBox="0 0 100 100" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+
+                </svg>
+
+                <div className="wraper flex direction-col justify-center">
+                    <div className="column flex">
+                        <div className="rule hidden-sm-down" ref="rule"></div>
+                        <h1 ref="heading" className="animate fadeIn delay">Digital <br />
+                            Creative Agency</h1>
+                    </div>
+                </div>
+                <ScrollButton />
+            </div>
+        )
+    }
 }
 
 export default Home
