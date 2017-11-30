b675b69ab13b2ba714f57ecc328c0d884e2ff314
Filip <kasardaafilip@gmail.com>
2017-11-30 18:43:03 +0100

    Animation Between Pages v2

--- src/app/Side/index.js
+++ src/app/Side/index.js
@@ -5,7 +5,7 @@ import './Side.css'
 
 function Side(props) {
     return (
-        <div id="Side" className="view">
+        <div id="Side" className="view flex flex-center">
             <h2>{props.page}</h2>
         </div>
     )
