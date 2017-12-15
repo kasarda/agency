928d4ca2048c7b7cb10d3eaaf1be7e79617572c0
Filip <kasardaafilip@gmail.com>
2017-12-15 16:09:29 +0100

    Update

--- src/app/Showcase/index.js
+++ src/app/Showcase/index.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 
 import './Showcase.css'
 class Showcase extends Component {
+
     render() {
         const { name } = this.props.match.params
 
         return (
-            <h1>ShowCase {name}</h1>
+            <div id="Showcase">
+
+            </div>
         )
     }
+
 }
 
 export default Showcase
