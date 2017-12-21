b852b6589473a04fd7dd05f0ec9c8acd3660986e
Filip <kasardaafilip@gmail.com>
2017-12-21 22:19:02 +0100

    Update

--- src/app/Contacts/index.js
+++ src/app/Contacts/index.js
@@ -15,19 +15,44 @@ class Contacts extends Component {
 
     render() {
         return (
-            <div id="Contacts" className="flex flex-center direction-col">
-                <Letter letter="C" />
+            <div id="Contacts" className="flex flex-center">
+                <div className="hidden-sm-down">
+                    <Letter letter="C" />
+                </div>
+
                 <div className="container">
-                    <h4 className="email">
+                    <h4 className="email typography">
                         <a href="mailto:info@digital.com">info@digital.com</a>
                     </h4>
-                    <address>
-                        Revierstredet 2 <br />
-                        NO-0104 OSLO <br />
-                        Norway
+                    <address className="typography">
+                        Revierstredet 2 OSLO <br />
+                        NO-0104 Norway
                     </address>
-                    <div className="phone">
+                    <div className="phone typography">
                         (067) 85 000
+                    </div>
+                    <div className="button">
+                        <a className="button-primary">Contact Us</a>
+                    </div>
+
+                    <div className="icons flex justify-between">
+                        <a href="https://www.instagram.com">
+                            <svg width="18" height="18" viewBox="0 0 24 24">
+                                <use className="fill-light-color" xlinkHref="#icon-instagram" />
+                            </svg>
+                        </a>
+
+                        <a href="https://www.twitter.com">
+                            <svg width="18" height="18" viewBox="0 0 24 24">
+                                <use className="fill-light-color" xlinkHref="#icon-twitter" />
+                            </svg>
+                        </a>
+
+                        <a href="https://www.youtube.com">
+                            <svg width="18" height="18" viewBox="0 0 24 24">
+                                <use className="fill-light-color" xlinkHref="#icon-youtube" />
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
