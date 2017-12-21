856a007fe8abe3c719e09ade66aa4af0df355bd9
Filip <kasardaafilip@gmail.com>
2017-12-21 14:34:21 +0100

    Modified

--- src/app/Contacts/index.js
+++ src/app/Contacts/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 
 import './Contacts.css'
 
+import Letter from '../Letter'
 class Contacts extends Component {
+
     componentDidMount() {
         document.body.dataset.preventTouch = "true"
     }
@@ -10,13 +12,26 @@ class Contacts extends Component {
     componentWillUnmount() {
         document.body.dataset.preventTouch = "false"
     }
+
     render() {
         return (
-            <div id="Contacts" className="flex flex-center">
-                <h1>Contacts</h1>
+            <div id="Contacts" className="flex flex-center direction-col">
+                <Letter letter="C" />
+                <div className="container">
+                    <h4 className="email">
+                        <a href="mailto:info@digital.com">info@digital.com</a>
+                    </h4>
+                    <address>
+                        Revierstredet 2 <br />
+                        NO-0104 OSLO <br />
+                        Norway
+                    </address>
+                    <div className="phone">
+                        (067) 85 000
+                    </div>
+                </div>
             </div>
         )
-
     }
 }
 
