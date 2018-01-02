945f088da663149d082652c8ee1657ba0bbd39d7
Filip <kasardaafilip@gmail.com>
2018-01-02 21:44:55 +0100

    Updated Build

--- src/services/local.js
+++ src/services/local.js
@@ -1,9 +1,9 @@
 import { HashRouter, BrowserRouter } from 'react-router-dom'
 
 const local = false
-const prod = process.env.NODE_ENV === 'producation'
+const dev = process.env.NODE_ENV === 'development'
 
 
 export const path = name => local ? '.' + name : name
-export const baseName = prod ? 'agency/build' : '/'
+export const baseName = dev ? '/' : 'agency/build'
 export const RouterType = local ? HashRouter : BrowserRouter
