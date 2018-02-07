3920d2d95d34d357493099f9c32001460e7cbe4b
Filip <kasardaafilip@gmail.com>
2018-02-07 16:35:32 +0100

    Replaced class component with function component

--- src/app/Navigation/index.js
+++ src/app/Navigation/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navigation.css'
 import { isPath } from '../../services/local'
+
 function Link({ to, name, icon }) {
     return (
         <NavLink exact to={to}>
@@ -15,28 +16,25 @@ function Link({ to, name, icon }) {
     )
 }
 
-class Navigation extends Component {
-    render() {
-        return (
-            <div id="Navigation">
-                <div className="flex wrap">
-                    <div className="column brand logo flex align-center">
-                        {
-                            isPath('/') ?
-                                <a aria-current="true" onClick={_ => this.props.onHomePage()}>Digital</a> :
-                                <NavLink to="/" activeClassName="">Digital</NavLink>
-                        }
-
-                    </div>
-                    <div className="column links link flex justify-end align-center">
-                        <Link to="/services" name="services" icon="icon-gear" />
-                        <Link to="/projects" name="projects" icon="icon-project" />
-                        <Link to="/contacts" name="contacts" icon="icon-mail" />
-                    </div>
+function Navigation({ onHomePage}) {
+    return (
+        <div id="Navigation">
+            <div className="flex wrap">
+                <div className="column brand logo flex align-center">
+                    {
+                        isPath('/') ?
+                            <a aria-current="true" onClick={_ => onHomePage()}>Digital</a> :
+                            <NavLink to="/" activeClassName="">Digital</NavLink>
+                    }
                 </div>
-            </div >
+                <div className="column links link flex justify-end align-center">
+                    <Link to="/services" name="services" icon="icon-gear" />
+                    <Link to="/projects" name="projects" icon="icon-project" />
+                    <Link to="/contacts" name="contacts" icon="icon-mail" />
+                </div>
+            </div>
+        </div >
         )
-    }
 }
 
 export default Navigation
