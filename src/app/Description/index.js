cd5a8f8e4b0f6c1463787d939a9b23970f190bb0
Filip <kasardaafilip@gmail.com>
2018-02-08 21:34:59 +0100

    Changed class component to functional

--- src/app/Description/index.js
+++ src/app/Description/index.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import preload from '../../services/preload'
 import './Description.css'
 
-class Description extends Component {
+function Description({ description, theme, id, onRedirect }) {
 
-    redirect(event) {
+    function redirect(event) {
         event.preventDefault()
-        this.props.onRedirect()
-
-        const { id } = this.props
+        onRedirect()
 
         const data = document.head.dataset
         const preloaded = data.preloaded || '|'
@@ -29,25 +27,22 @@ class Description extends Component {
 
     }
 
-    render() {
-        const { description, theme } = this.props
-        const descriptionList = description.map((text, key) =>
-            (<p key={key} className={`init-animation init-animation-${key + 1}`}>{text}</p>)
-        )
+    const descriptionList = description.map((text, key) =>
+        (<p key={key} className={`init-animation init-animation-${key + 1}`}>{text}</p>)
+    )
 
-        return (
-            <div id="Description">
-                <div className="paragraphs hidden-sm-down">
-                    {descriptionList}
-                </div>
-                <div className={`button init-animation init-animation-${descriptionList.length + 1}`}>
-                    <div className="primary-button" style={{ color: theme }}>
-                        <a onClick={this.redirect.bind(this)}>view case</a>
-                    </div>
+    return (
+        <div id="Description">
+            <div className="paragraphs hidden-sm-down">
+                {descriptionList}
+            </div>
+            <div className={`button init-animation init-animation-${descriptionList.length + 1}`}>
+                <div className="primary-button" style={{ color: theme }}>
+                    <a onClick={redirect}>view case</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Description
