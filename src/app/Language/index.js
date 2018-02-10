c5fde757653b63371cf37a8453b275082921e8ea
Filip <kasardaafilip@gmail.com>
2018-02-10 20:45:09 +0100

    Created language system

--- src/app/Language/index.js
+++ src/app/Language/index.js
@@ -6,13 +6,33 @@ class Language extends Component {
     constructor() {
         super()
 
-        this.state = {}
+        this.state = {
+            language: 'en'
+        }
+    }
+
+    onClick(language) {
+        this.setState({ language })
     }
 
     render() {
-
+        const { language } = this.state
         return (
-            <div id="Language"></div>
+            <div id="Language">
+                <div className="buttons flex justify-between">
+                    <div
+                        onClick={this.onClick.bind(this, 'en')}
+                        className={`button primary-hover ${language === 'en' && 'active'}`}
+                        role="button">EN</div>
+
+                    <div className="divider"></div>
+
+                    <div
+                        onClick={this.onClick.bind(this, 'sk')}
+                        className={`button primary-hover ${language === 'sk' && 'active'}`}
+                        role="button">SK</div>
+                </div>
+            </div>
         )
     }
 }
