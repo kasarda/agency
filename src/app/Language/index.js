0cb0f71a2ffdfeae5175afc2a78f8a22b3e2e94b
Filip <kasardaafilip@gmail.com>
2018-02-10 20:01:57 +0100

    Initialize Language component

---
+++ src/app/Language/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Language.css'
+
+class Home extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {}
+    }
+
+    render() {
+
+        return (
+            <div id="Language"></div>
+        )
+    }
+}
+
+export default Home
