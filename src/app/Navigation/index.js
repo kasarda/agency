41e69166e68405a9fb12e61e7f62340a5434b201
Filip <kasardaafilip@gmail.com>
2018-02-12 18:04:14 +0100

    Modified and applied secondary hover

--- src/app/Navigation/index.js
+++ src/app/Navigation/index.js
@@ -9,7 +9,7 @@ import './Navigation.css'
 function Link({ to, name, icon }) {
     return (
         <NavLink exact to={to}>
-            <span className="hidden-sm-down">{name}</span>
+            <span className="hidden-sm-down secondary-hover" data-text={name}>{name}</span>
             <span className="hidden-md-up">
                 <Icon name={icon}/>
             </span>
@@ -18,20 +18,21 @@ function Link({ to, name, icon }) {
 }
 
 function Navigation({ onHomePage }) {
+    const brand = <span className="secondary-hover" data-text="digital">Digital</span>
     return (
         <div id="Navigation">
             <div className="flex wrap">
                 <div className="column brand logo flex align-center">
                     {
                         isPath('/') ?
-                            <a aria-current="true" onClick={_ => onHomePage()}>Digital</a> :
-                            <NavLink to="/" activeClassName="">Digital</NavLink>
+                            <a aria-current="true" onClick={_ => onHomePage()}>{brand}</a> :
+                            <NavLink to="/" activeClassName="">{brand}</NavLink>
                     }
                 </div>
                 <div className="column links link flex justify-end align-center">
-                    <Link to="/services" name={text('services', 'servis')} icon="gear" />
-                    <Link to="/projects" name={text('projects', 'projekty')} icon="project" />
-                    <Link to="/contacts" name={text('contacts', 'kontakt')} icon="mail" />
+                    <Link to="/services" name={text('services', 'servis', true)} icon="gear" />
+                    <Link to="/projects" name={text('projects', 'projekty', true)} icon="project" />
+                    <Link to="/contacts" name={text('contacts', 'kontakt', true)} icon="mail" />
                 </div>
             </div>
         </div >
