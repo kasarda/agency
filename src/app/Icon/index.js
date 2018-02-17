90886ffe27272f40dcf77d720a35d18841da3ede
Filip <kasardaafilip@gmail.com>
2018-02-17 14:33:54 +0100

    Fixed issue with icon class when if light was undefined returned "undefined" class

--- src/app/Icon/index.js
+++ src/app/Icon/index.js
@@ -4,7 +4,7 @@ function Icon({ name, light, className, width = 18, height = 18, onClick }) {
 
     return (
         <svg width={width} height={height} viewBox="0 0 24 24" className={className} onClick={e => onClick && onClick(e)}>
-            <use className={`icon ${light && 'fill-light-color'}`} xlinkHref={'#icon-' + name} />
+            <use className={`icon ${light ? 'fill-light-color' : ''}`} xlinkHref={'#icon-' + name} />
         </svg>
     )
 }
